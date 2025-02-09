038a882f52108803f10e08e3a4eab2f048ea2fce
Damien Gobillot <damien.gobillot@gmail.com>
2025-02-09 12:55:45 +0100

    Mise à jour initialisation du labyrinthe complet

--- lbo.py
+++ lbo.py
@@ -1,16 +1,32 @@
 import pygame
+import lbo_tree
 
 # Initialisation de Pygame
 pygame.init()
 
-# Définition des paramètres de la fenêtre
-WIDTH, HEIGHT = 800, 600
+# Définition des paramètres généraux du labyrinthe
+screen_w = 800
+screen_h = 600
+lbo_h=10
+lbo_w=20
+box_h=screen_h//lbo_h
+box_w=screen_w//lbo_w
+
+WIDTH, HEIGHT = screen_w, screen_h
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Mon premier jeu Pygame")
+
+pygame.display.set_caption("Mon premier jeu grâce à Pygame !")
 
 # Définition des couleurs
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+actual_color = BLUE
+current_pos_x = 0
+current_pos_y = 0
 
 # Boucle principale
 running = True
@@ -22,9 +38,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Dessiner un rectangle bleu
-    pygame.draw.rect(screen, BLUE, (100, 100, 50, 50))
-
+    # Dessiner le point de départ (0,0 par défaut)
+    for i in range(0,lbo_h,1):
+        for j in range(0,lbo_w,1):
+            pygame.draw.rect(screen, actual_color, (current_pos_x, current_pos_y, box_w, box_h))
+            pygame.draw.rect(screen, BLACK, (current_pos_x, current_pos_y, box_w, box_h),3)
+            current_pos_x += box_w
+            actual_color = WHITE
+            print("passage 1 : ",current_pos_x)
+        current_pos_y += box_h
+        current_pos_x = 0
+    actual_color = BLUE
+    current_pos_x = 0
+    current_pos_y = 0
     # Mise à jour de l'affichage
     pygame.display.flip()
 
